347d7a89feeb32b18ca3c329051d73e2b4c9de89
Matt Burke <spraints@gmail.com>
2011-08-09 11:52:26 -0400

    Access env value CONFIG_A_B_C as config.a.b.c or config.a_b_c and convert types more like yaml does (so 'false' will be false).

--- lib/static_config/reader/environment.rb
+++ lib/static_config/reader/environment.rb
@@ -16,12 +16,36 @@ module StaticConfig
       def update(data, env_name, env_value)
         if env_name =~ /^#{@env_prefix}_(.*)/
           parts = $1.downcase.split(/_/)
-          last_part = parts.pop
-          parts.inject(data) { |h, k| h[k] ||= {} }[last_part] = env_value
+          value = convert(env_value)
+          build(data, parts, value)
         end
         data
       rescue => e
         raise "Unable to fit #{env_name} into #{data.inspect}"
+      end
+
+      def build(data, keys, value)
+        first, *rest = keys
+        while !rest.empty?
+          build(data[first] ||= {}, rest, value)
+          first += '_' + rest.shift
+        end
+        data[first] = value
+      end
+
+      def convert env_value
+        case env_value
+        when /^\d+$/
+          env_value.to_i
+        when /^(\d*\.\d+)|(\d+\.\d*)$/
+          env_value.to_f
+        when 'true'
+          true
+        when 'false'
+          false
+        else
+          env_value
+        end
       end
     end
   end
--- spec/static_config/reader/environment_spec.rb
+++ spec/static_config/reader/environment_spec.rb
@@ -2,15 +2,49 @@ require 'spec_helper'
 require 'static_config/reader/environment'
 
 describe StaticConfig::Reader::Environment do
-  before do
-    ENV['MY_CONFIG_DEV_KEY'] = 'value'
+  def self.has_env(e)
+    before do
+      @original_env ||= {}
+      e.each do |key, value|
+        @original_env[key], ENV[key] = ENV[key], value
+      end
+    end
+    after do
+      e.each do |key, _|
+        ENV[key] = @original_env[key]
+      end
+    end
   end
-  after do
-    ENV['MY_CONFIG_DEV_KEY'] = nil
-  end
-  let(:subject) { described_class.read(opts) }
 
+  let(:subject) { described_class.read(opts) }
   def opts ; { :env_prefix => 'MY_CONFIG' } ; end
-  it('reads environment') { should == { 'dev' => { 'key' => 'value' } } }
+
+  context do
+    has_env 'MY_CONFIG_TEST' => 'value'
+    it('reads environment') { should == { 'test' => 'value' } }
+  end
+
+  context do
+    has_env 'MY_CONFIG_A_B_C' => 'D'
+    it { subject['a_b_c'].should == 'D' }
+    it { subject['a']['b_c'].should == 'D' }
+    it { subject['a_b']['c'].should == 'D' }
+    it { subject['a']['b']['c'].should == 'D' }
+  end
+
+  context do
+    has_env({
+      'MY_CONFIG_INT' => '123',
+      'MY_CONFIG_TRUE' => 'true',
+      'MY_CONFIG_FALSE' => 'false',
+      'MY_CONFIG_FLOAT' => '123.45',
+      'MY_CONFIG_STRING' => 'string',
+    })
+    it { subject['int'].should == 123 }
+    it { subject['true'].should == true }
+    it { subject['false'].should == false }
+    it { subject['float'].should == 123.45 }
+    it { subject['string'].should == 'string' }
+  end
 end
 
